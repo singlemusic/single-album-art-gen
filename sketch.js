b8d4ade0a8c72239d4c54f65fd005046fdd5a6ce
J. Taylor O'Connor <taylor@singlemusic.co>
2018-09-15 17:18:32 -0500

    tweaks

--- sketch.js
+++ sketch.js
@@ -3,37 +3,43 @@ var sketch = function (p) {
 
   p.setup = function () {
     p.createCanvas(1600, 1600);
-    p.noLoop();
+    p.noLoop(); // don't draw until generate is clicked.
 
-    textFont = p.loadFont('fonts/Montserrat-Medium.ttf')
+    textFont = p.loadFont('fonts/Montserrat-Medium.ttf');
 
+      // generate click handler
+      p.select("#generate").mousePressed(function () {
+      // start the draw() loop.
+      p.loop();
+    });
 
-
-    // register generate click handler
-    p.select("#generate").mousePressed(p.generate);
-
-    p.select("#download").mousePressed(function() {
+    // download click handler
+    p.select("#download").mousePressed(function () {
       var canvas = document.getElementById("defaultCanvas0");
       var download = document.getElementById("download");
       download.download = new Date().getUTCMilliseconds + ".png";
       var image = canvas.toDataURL("image/png")
         .replace("image/png", "image/octet-stream");
-      window.location.href=image;
+      window.location.href = image;
     });
   }
 
+  var rotation = 0;
   p.draw = function () {
+    // grab all our colors
     var color1 = p.select("#color_1").value();
     var color2 = p.select("#color_2").value();
     var color3 = p.select("#color_3").value();
     var textColor = p.select("#color_4").value();
     var background = p.select("#color_5").value();
+
     p.background(background);
     var name = p.select('#band_name').value();
     if (name) {
       for (var i = 0; i < 5; i++) {
         p.drawPolygon([color1, color2, color3]);
       }
+
       p.fill(textColor);
       p.noStroke();
       p.textFont(textFont);
@@ -43,11 +49,6 @@ var sketch = function (p) {
     p.noLoop();
   }
 
-  p.generate = function () {
-    // start the draw() loop.
-    p.loop();
-  }
-
   p.drawPolygon = function (colors) {
     let color = p.randColor(colors);
     if (p.random() < 0.3) {
@@ -55,7 +56,7 @@ var sketch = function (p) {
       p.noStroke();
     } else {
       p.stroke(color);
-      p.strokeWeight(p.random(5, 15));
+      p.strokeWeight(p.random(10, 30));
       p.noFill();
     }
     var sides = p.random(3, 6);
